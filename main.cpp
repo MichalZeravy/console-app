71b65c2e8b1f7a0a1f063de81fd38cdd76e37fa3
Michal Žeravý <zeravymichal@gmail.com>
2018-09-25 11:33:17 +0200

    Zadanie 2

--- main.cpp
+++ main.cpp
@@ -2,13 +2,51 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <iterator>
 
-int main()
+using namespace std;
+
+int main(int argc, char *argv[])
 {
-	char c;
-	std::cout << "Hello World!" << std::endl;
-	std::cin >> c;
-	std::cout << c << std::endl;
+	int n, it;
+	string str;
+	if (argc == 1)
+		cout << "Nebol zadany ziaden parameter";
+	if (argc == 2)
+	{
+		if ("-c" == (string)argv[argc - 1])
+		{
+			getline(cin, str);
+			n = str.length();
+			cout << n << endl;
+			//std::istream s(std::string(argv[1]));
+			//std::istream_iterator<char> it = s->begin();
+		}
 
+		if ("-w" == (string)argv[argc - 1])
+		{
+			getline(cin, str);
+			cout << str;
+			int s;
+			n = str.length();
+			for (int i = 0; i < n; i++)
+				if (isspace(str[i]))
+					s++;
+			cout << s;
+
+
+		}
+		//funkcnost plus nastudovat ako nacitavat viacej riadkov 
+		if ("-l" == (string)argv[argc - 1])
+		{
+			getline(cin, str);
+			cout << "-l";
+		}
+			
+	}
+	system("pause");
+	
 	return 0;
 }
