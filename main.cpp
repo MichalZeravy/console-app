3214d94761f1483537fda6b6984d4700beb7e218
Michal Žeravý <zeravymichal@gmail.com>
2018-10-01 14:35:25 +0200

    Zadanie c.2

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int n, it;
-	string str;
+	int n,it;
+	string str, line;
 	if (argc == 1)
 		cout << "Nebol zadany ziaden parameter";
 	if (argc == 2)
@@ -20,29 +20,39 @@ int main(int argc, char *argv[])
 		{
 			getline(cin, str);
 			n = str.length();
-			cout << n << endl;
+			cout <<"Pocet znakov je: "<< n << endl;
 			//std::istream s(std::string(argv[1]));
 			//std::istream_iterator<char> it = s->begin();
 		}
 
 		if ("-w" == (string)argv[argc - 1])
 		{
+			int s=0;
 			getline(cin, str);
-			cout << str;
-			int s;
 			n = str.length();
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < n-1; i++)
 				if (isspace(str[i]))
 					s++;
-			cout << s;
+			if (s == 0 && isalpha(str[0]))
+				s = 1;
+
+			cout <<"Pocet slov je: "<< s;
 
 
 		}
 		//funkcnost plus nastudovat ako nacitavat viacej riadkov 
 		if ("-l" == (string)argv[argc - 1])
 		{
-			getline(cin, str);
-			cout << "-l";
+			int s = 0;
+			while (getline(cin, line)) 
+			{
+				s++;
+				if (line == "^")
+					break;
+
+				str+= " " + line;
+			}
+			cout << "Pocet riadkov je:" << s <<endl;
 		}
 			
 	}
